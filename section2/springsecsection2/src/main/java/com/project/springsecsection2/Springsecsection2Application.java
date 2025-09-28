45956c7e24e00b8ed0183b63cdc47fdab6c1f9aa
amnsuryansh <princesuryansh2000@gmail.com>
2025-09-28 16:47:30 +0530

    section2 actual intial commit

---
+++ section2/springsecsection2/src/main/java/com/project/springsecsection2/Springsecsection2Application.java
@@ -0,0 +1,15 @@
+package com.project.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = {"com.project.springsecsection1.controller"})
+public class Springsecsection2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecsection1Application.class, args);
+	}
+
+}
